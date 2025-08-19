f70f52e1e9704c9037598902b6c131451e183c12
saif <211128@ppu.edu.ps>
2025-08-19 22:26:37 +0300

    this is first commit

---
+++ tast.py
@@ -0,0 +1,30 @@
+name = input("enter name")
+age = input("enter age")
+boot_camp = input("enter name boot camp")
+with open(file.txt , w ) as f
+f.write(name)
+f.write(age)
+f.write(boot_camp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
