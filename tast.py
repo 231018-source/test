e7d074fd8d50ff94d7e49b05d2b3a99cb06ea274
saif <211128@ppu.edu.ps>
2025-08-21 21:53:32 +0300

    task3

--- tast.py
+++ tast.py
@@ -1,15 +1,35 @@
-name = input("Enter name: ")
-age = input("Enter age: ")
-boot_camp_name = input("Enter boot camp name: ")
-with open("file.txt", "w") as f:
-    f.write(name + "\n")
-    f.write(age + "\n")
-    f.write(boot_camp_name + "\n")
-    print("done saved notes")
-    with open("file.txt","r") as f:
-     reads = f.read()
-     print(reads)
-        
+# name = input("Enter name: ")
+# age = input("Enter age: ")
+# boot_camp_name = input("Enter boot camp name: ")
+# with open("file.txt", "w") as f:
+#     f.write(name + "\n")
+#     f.write(age + "\n")
+#     f.write(boot_camp_name + "\n")
+#     print("done saved notes")
+#     with open("file.txt","r") as f:
+#      reads = f.read()
+#      print(reads)
+       
+       
+#1
+fruits = ["Apple", "Banana", "Orange", "Grapes", "Strawberry"]
+print( fruits)
+#2
+print( fruits[0])
+print( fruits[-1])
+#3
+fruits[1] = "Mango"
+print( fruits)
+#4
+fruits.insert(2, "Watermelon")
+print( fruits)
+#5
+fruit = input("enter name fruits")
+print(fruits.count(fruit) != 0)
+#6
+fruits.sort()
+print(fruits)
+ 
 
 
 
