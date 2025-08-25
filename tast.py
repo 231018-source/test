9a5c83846f13d1f9dd7f40660aba58fe708eb7b8
saif <211128@ppu.edu.ps>
2025-08-25 17:39:30 +0300

    task4

--- tast.py
+++ tast.py
@@ -18,25 +18,22 @@
   
   
   
-# task2     
-username = input(" enter a username :")
-password = input(" enter a password :")
+# # task2     
+# username = input(" enter a username :")
+# password = input(" enter a password :")
 
-username_fixed = username.lower()
-username_fixed = username_fixed.replace(" ", "_")
-password_length = len(password)
-print("\n--- Print all results---")
-print(f" Username after modification :{username_fixed}")
-print(f" length of the password : {password_length}")
+# username_fixed = username.lower()
+# username_fixed = username_fixed.replace(" ", "_")
+# password_length = len(password)
+# print("\n--- Print all results---")
+# print(f" Username after modification :{username_fixed}")
+# print("\n--- -Use Boolean expressions to check:--")
+# print(" password length is greater than or equal to 8 : ", password_length >= 8)
+# print("username is admin : ", username_fixed == "admin")
+# print(" password is 1234 :", password == "1234")
+# print("default account :", username_fixed == "admin" and password == "1234")
 
-
-print("\n--- -Use Boolean expressions to check:--")
-print(" password length is greater than or equal to 8 : ", password_length >= 8)
-print("username is admin : ", username_fixed == "admin")
-print(" password is 1234 :", password == "1234")
-print("default account :", username_fixed == "admin" and password == "1234")
-
-print(f"\n heloo {username_fixed}، password length{password_length} ")
+# print(f"\n heloo {username_fixed}، password length{password_length} ")
 
      
      
@@ -66,8 +63,19 @@ print(f"\n heloo {username_fixed}، password length{password_length} ")
 
 
 
-
-
+#task4
+fruit_prices = {
+    "apple": 2.5,
+    "banana": 1.2,
+    "orange": 1.8,
+    "mango": 3.0,
+    "grape": 2.0
+}
+fruit_name = input("Enter the name of a fruit: ").lower()
+if fruit_name in fruit_prices:
+    print(f"The price of {fruit_name} is ${fruit_prices[fruit_name]}")
+else:
+    print("Sorry, this fruit is not available.")
 
 
 
